7ce7601808959fc49d592e73cfc275a0c5770493
Antonio <antonioareas@me.com>
2020-10-16 17:57:53 -0700

    Add project files.

---
+++ RockPaperScissors/Game.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissors {
+    class Game {
+
+        /// <summary>
+        /// Check who won.
+        /// </summary>
+        /// <param name="answer">Rock, Paper or Scissors</param>
+        /// <returns>Status of game</returns>
+        public char CheckAnswer(string answer) {
+
+            //get answer from player
+            int computerAnswer = ComputerAnswer();
+
+            //rock = 1, paper = 2, scissors = 3
+            switch (answer) {
+
+                case "rock":
+                    if (computerAnswer == 1) {
+                        return 't';
+                    }
+                    else if (computerAnswer == 2) {
+                        return 'l';
+                    }
+                    else {
+                        return 'w';
+                    }
+
+                case "paper":
+                    if (computerAnswer == 1) {
+                        return 'w';
+                    }
+                    else if (computerAnswer == 2) {
+                        return 't';
+                    }
+                    else {
+                        return 'l';
+                    }
+
+                case "scissors":
+                    if (computerAnswer == 1) {
+                        return 'l';
+                    }
+                    else if (computerAnswer == 2) {
+                        return 'w';
+                    }
+                    else {
+                        return 't';
+                    }
+
+                default:
+                    return 'n';
+
+            }
+
+        }
+
+        /// <summary>
+        /// generate computer answer
+        /// </summary>
+        /// <returns>computer's answer</returns>
+        private int ComputerAnswer() {
+
+            Random generator = new Random();
+            return generator.Next(1, 4);
+
+        }
+
+    }
+}
---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace RockPaperScissors {
+    class Program {
+        static void Main(string[] args) {
+
+            //create game
+            Game game = new Game();
+            int gamesPlayed = 0;
+            int computerWins = 0;
+            int playerWins = 0;
+
+            // check who won and add to counter
+            do {
+
+                Console.WriteLine("Choose Rock, Paper or Scissors");
+                string answer = Console.ReadLine().ToLower();
+
+                char result = game.CheckAnswer(answer);
+
+                switch (result) {
+
+                    case 'w':
+                        Console.WriteLine("You win.");
+                        playerWins++;
+                        break;
+
+                    case 'l':
+                        Console.WriteLine("You lose.");
+                        computerWins++;
+                        break;
+
+                    case 't':
+                        Console.WriteLine("You tied.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Command not recognized");
+                        gamesPlayed--;
+                        break;
+
+                }
+
+                gamesPlayed++;
+
+            } while (gamesPlayed < 5);
+
+            //shows wins and losses
+            Console.WriteLine("\n\nPlayer Wins: " + playerWins + "\nComputer Wins: " + computerWins);
+
+        }
+    }
+}
